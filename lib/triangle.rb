cd71170e2f9adaf416a7011cf6125a74a251513e
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-12 18:35:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,9 @@ class Triangle
 
   def kind
       validate_triangle
-      if a == b && b == c
+      if side_1 == side_2 && side_2 == side_3
         :equilateral
-      elsif a == b || b == c || a == c
+      elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
         :isosceles
       else
         :scalene
@@ -18,8 +18,8 @@ class Triangle
     end
 
     def validate_triangle
-      real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-      [a, b, c].each do |side|
+      real_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
+      [side_1, side_2, side_3].each do |side|
         real_triangle << false if side <= 0
       raise TriangleError if real_triangle.include?(false)
       end
