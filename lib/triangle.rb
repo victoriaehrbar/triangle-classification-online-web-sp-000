432af5235d5c97b154953b72c814d25c3766ab54
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-12 18:33:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,4 +5,28 @@ class Triangle
     @side_2 = side_2
     @side_3 = side_3
   end
-end
+
+  def kind
+      validate_triangle
+      if a == b && b == c
+        :equilateral
+      elsif a == b || b == c || a == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+    def validate_triangle
+      real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+      [a, b, c].each do |side|
+        real_triangle << false if side <= 0 
+      raise TriangleError if real_triangle.include?(false)
+      end
+    end
+
+    class TriangleError < StandardError
+    end
+
+  end
+
